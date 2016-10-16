2af860817c65c083375a07fad9a8b21b8bebbf4b
Ashar Fuadi <fushar@gmail.com>
2016-10-16 10:14:35 +0100

    Finalize ashar's panitia solution

--- final/panitia/solution_ashar.cpp
+++ final/panitia/solution_ashar.cpp
@@ -6,24 +6,39 @@ const int MAX = 55;
 int T;
 int N, M;
 int C[MAX], P[MAX];
-double comb[105][105];
+double dp[MAX][MAX][MAX];
+double prob[MAX];
 
-double expected_value(int c) {
-	double res = 0;
-	for (int i = 0; i <= P[c] && i <= N; i++) {
-		res += min(i, C[c]) * comb[P[c]][i] * comb[M-i][N-i];
+double compute_prob(const vector<pair<int, int>>& combs) {
+	vector<int> num, denom;
+
+	for (int x = 0; x < 2; x++) {
+		for (int i = 1; i <= combs[x].second; i++) {
+			denom.push_back(i);
+			num.push_back(combs[x].first-i+1);
+		}
 	}
-	return res / comb[M][N];
+	for (int i = 1; i <= combs[2].second; i++) {
+		num.push_back(i);
+		denom.push_back(combs[2].first-i+1);
+	}
+
+	double res = 1;
+	int nn = num.size(), nd = denom.size();
+	int in = 0, id = 0;
+	while (in < nn || id < nd) {
+		if (res > 1 && id < nd) {
+			res /= denom[id++];
+		} else if (in < nn) {
+			res *= num[in++];
+		} else if (id < nd) {
+			res /= denom[id++];
+		}
+	}
+	return res;
 }
 
 int main() {
-	for (int i = 0; i <= 100; i++) {
-		comb[i][0] = 1;
-		for (int j = 1; j <= i; j++) {
-			comb[i][j] = comb[i-1][j] + comb[i-1][j-1];
-		}
-	}
-
 	scanf("%d", &T);
 	for (int tc = 0; tc < T; tc++) {
 		scanf("%d", &N);
@@ -39,11 +54,36 @@ int main() {
 			M += P[c];
 		}
 
-		double x = 0;
-		for (int c = 0; c <= N; c++) {
-			x += expected_value(c);
+		memset(dp, 0, sizeof dp);
+		dp[0][0][0] = 1;
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) {
+				int n = N-j;
+
+				for (int k = 0; k <= P[i] && k <= n; k++) {
+					prob[k] = compute_prob({ {P[i], k}, {M-P[i], n-k}, {M, n} });
+					//prob[k] = comb[P[i]][k] * comb[M-P[i]][n-k] / comb[M][n];
+				}
+
+				for (int k = 0; k <= j; k++) {
+					if (!dp[i][j][k]) {
+						continue;
+					}
+
+					for (int l = 0; l <= P[i] && l <= n; l++) {
+						int kena = min(C[i], l);
+						dp[i+1][j+l][k+kena] += prob[l] * dp[i][j][k];
+					}
+				}
+			}
+			M -= P[i];
 		}
 
-		printf("%.5lf\n", x / (2*N - x));
+		double res = 0;
+		for (int k = 0; k <= N; k++) {
+			res += dp[N+1][N][k] * k / (2*N - k);
+		}
+
+		printf("%.3lf\n", res);
 	}
 }
