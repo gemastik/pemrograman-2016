46263c008b3025e4c86d827c7742a90654a36aec
Alham Fikri Aji <afaji321@gmail.com>
2016-10-17 18:56:16 +0900

    add pohon solution

---
+++ final/pohon/solution_aji.cpp
@@ -0,0 +1,92 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+class node{
+private:
+	vector<node*> adj;
+
+public:
+	int dp[2][2];
+	void add_child(node *c){
+		adj.push_back(c);
+	}
+
+	node(){
+		memset(dp,-1,sizeof dp);
+	}
+
+	int size(){
+		return adj.size();
+	}
+
+	node* get(int id){
+		return adj[id];
+	}
+};
+
+node** all_node;
+
+
+int dfs(node* pos,node* par,int state,int state_par){
+	cout<<pos<<endl;
+	if (pos->dp[state][state_par] != -1)
+		return pos->dp[state][state_par];
+
+	int &tmp = pos->dp[state][state_par];
+	tmp = 0;
+	int anak = pos->size();
+	for (int i=0;i<anak;i++){
+		if (par == pos->get(i))
+			continue;
+
+		int set_red = 199999;
+		int set_black = 199999;
+		if ((state != 0 || state_par != 0) && anak <= 2)
+			set_red = dfs(pos->get(i) , pos,0, state);
+		if ((state != 1 || state_par != 1) && anak <= 2)
+			set_black = 1 + dfs(pos->get(i) , pos,1, state);
+		if (anak > 2){
+			if (state == 0)
+				set_black = 1 + dfs(pos->get(i) , pos,1, state);
+			else
+				set_red = dfs(pos->get(i) , pos,1, state);
+		}
+		
+		tmp += min(set_black, set_red);
+	}
+	return tmp;
+}
+
+int main() {
+	int N,T;
+	scanf("%d",&T);
+	while(T--){
+		scanf("%d",&N);
+		all_node = new node*[N];
+
+		for (int i=0;i<N;i++){
+			all_node[i] = new node();
+		}
+
+		for (int i=1;i<N;i++){
+			int a,b;
+			scanf("%d%d",&a,&b);
+			a--;
+			b--;
+			all_node[a]->add_child(all_node[b]);
+			all_node[b]->add_child(all_node[a]);
+		}
+		if (N == 3)
+			cout<<1<<endl;
+		else if(N < 3)
+			cout<<0<<endl;
+		else
+			cout<<min(min(dfs(all_node[0],0,0,0) , dfs(all_node[0],0,0,1)) ,1 + min(dfs(all_node[0],0,1,0) , dfs(all_node[0],0,1,1)) )<<endl;
+		delete[] all_node;
+	}
+}
