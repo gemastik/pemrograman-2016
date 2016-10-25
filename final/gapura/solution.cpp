9b3412238445243032d7c477f32ffca32bfb96df
Alham Fikri Aji <afaji321@gmail.com>
2016-10-25 21:59:53 +0900

    fixed overflow in solution

--- final/gapura/solution.cpp
+++ final/gapura/solution.cpp
@@ -8,6 +8,7 @@ int sum;
 long long sol;
 int N,T,P;
 int tiang[30];
+long long aa,bb;
 
 void cout_opsi(int p1,int p2){
 	printf("   ( %d x %d ) = %d\n",p1+p2, (sum-p1-p2)/2 ,(p1+p2) * (sum-p1-p2)/2);
@@ -20,14 +21,25 @@ void find_best(int tinggi, int panjang){
 	//cout<<" cari di "<<best_panjang - panjang<<endl;
 	if (solutions[tinggi].size() == 0)
 		return;
-
+	int Z = solutions[tinggi].size();
 	int id = lower_bound(solutions[tinggi].begin(), solutions[tinggi].end(), best_panjang - panjang) - solutions[tinggi].begin();
-	if (id < N){
-		sol = max(sol, (long long)(solutions[tinggi][id] + panjang)* (sum - solutions[tinggi][id] - panjang)/2);
+	if (id < Z){
+		long long tmp = (long long)(solutions[tinggi][id] + panjang)* (long long)(sum - solutions[tinggi][id] - panjang)/2LL;
+		if (tmp > sol){
+			sol = tmp;
+			aa = (solutions[tinggi][id] + panjang);
+			bb = (sum - solutions[tinggi][id] - panjang)/2LL;
+		}
 		//cout_opsi(panjang,solutions[tinggi][id]);
 	}
 	if (id - 1 > 0){
-		sol = max(sol, (long long)(solutions[tinggi][id - 1] + panjang) * (sum - solutions[tinggi][id - 1] - panjang)/2);
+		long long tmp = (long long)(solutions[tinggi][id - 1] + panjang) * (long long)(sum - solutions[tinggi][id - 1] - panjang)/2LL;
+
+		if (tmp > sol){
+			sol = tmp;
+			aa = (solutions[tinggi][id-1] + panjang);
+			bb = (sum - solutions[tinggi][id-1] - panjang)/2LL;
+		}
 		//cout_opsi(panjang,solutions[tinggi][id]);
 	}
 }
@@ -60,8 +72,9 @@ int main() {
 		for (int i=0;i<N;i++){
 			scanf("%d",&tiang[i]);
 			sum += tiang[i];
+			//cout<<tiang[i]<<endl;
 		}
-	
+		
 		best_panjang = sum / 2;
 		//meet in the middle 
 		P = N/2;
